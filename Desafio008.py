e3e1bf4e62c619016f81b3c8770cf201224079e5
Kelvin Harlan <kelvinharlandev@gmmail.com>
2022-06-24 12:20:10 -0300

    Convertendo Metros em CM e em mm

---
+++ Desafio008.py
@@ -0,0 +1,6 @@
+nome = input('Olá, Digite seu nome: ')
+metros = float (input('Digite um valor em Metros: '))
+centimetros = (metros*100)
+milimetros = (metros*1000)
+print(f'{metros} Metros tem:\n{centimetros} Centímetros\n{milimetros} Milímetros')
+
